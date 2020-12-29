9c648af6b038883ac926a1bbf24f87d4ac9069b6
unknown <duclosxkevin@hotmail.fr>
2020-12-29 11:39:35 +0100

    home

--- wp-content/themes/redlab-blog/home.php
+++ wp-content/themes/redlab-blog/home.php
@@ -1,5 +1,22 @@
 <?php get_header(); ?>
-
-<h1>Salut</h1>
+<div class="container">
+    <h1>Salut</h1>
+    <main class="main">
+        <div class="sticky">
+            <img src="" alt="">
+        </div>
+        <div class="content">
+            <?php if(have_posts()) : ?>
+                <?php while (have_posts())  : the_post(); ?>
+                    <div class="card">
+                        <div class="card-title">
+                            <?php the_title(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+    </main>
+</div>
 
 <?php get_footer(); ?>
