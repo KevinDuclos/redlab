41a3d3a4b99d408268dcb6a656e8b256ff689fe1
unknown <duclosxkevin@hotmail.fr>
2021-01-07 22:35:19 +0100

    commit

--- wp-content/themes/redlab-blog/home.php
+++ wp-content/themes/redlab-blog/home.php
@@ -6,15 +6,19 @@
             <img src="" alt="">
         </div>
         <div class="content">
-            <?php if(have_posts()) : ?>
-                <?php while (have_posts())  : the_post(); ?>
-                    <div class="card">
-                        <div class="card-title">
-                            <?php the_title(); ?>
+            <div class="row">
+                <?php if(have_posts()) : ?>
+                    <?php while (have_posts())  : the_post(); ?>
+                        <div class="col-4">
+                            <div class="card">
+                                <div class="card-title">
+                                    <?php the_title(); ?>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                <?php endwhile; ?>
-            <?php endif; ?>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
         </div>
     </main>
 </div>
