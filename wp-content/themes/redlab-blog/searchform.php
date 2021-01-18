a71ba5b862a4d9907a57257597fe561eac31f40e
aegir.boissel <aegir.boissel@docapost-agility.fr>
2021-01-18 22:44:27 +0100

    y a du rouge et des loupes dans ce monde de brutes

---
+++ wp-content/themes/redlab-blog/searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div class="searchWrap">
+        <input type="text" value="" name="s" id="s" placeholder="Search" />
+        <i class="fas fa-search"></i>
+    </div>
+</form>
